6a63bf8795f825aa36b41d2581e2607147a83649
Wojtek Zalewski <wojtek@neverbland.com>
2014-01-27 14:40:49 +0100

    Travis and Coveralls configs included Dummy response class

---
+++ src/Wtk/Response.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package Response
+ *
+ * @author Wojtek Zalewski <wojtek@neverbland.com>
+ *
+ * @copyright Copyright (c) 2013, Wojtek Zalewski
+ * @license MIT
+ */
+
+namespace Wtk\Response;
+
+/**
+ * Response class.
+ *
+ * @author Wojtek Zalewski <wojtek@neverbland.com>
+ */
+class Response implements ResponseInterface
+{
+    /**
+     * Dummy
+     */
+}
